8d542940dafbc4bf328bf22d7cfc11dda56780b8
Palkesh-7 <palkeshmalviya.pm@gmail.com>
2024-11-30 13:09:41 +0530

    create protfolio

---
+++ src/data/constants.js
@@ -0,0 +1,376 @@
+export const Bio = {
+  name: "Palkesh Malviya",
+  roles: [
+    "AI Engineer",
+    "Python Developer",
+    "Go Developer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Palkesh-7",
+  resume:
+    "https://drive.google.com/file/d/1WVqaYk14nfuicOiDqVFOjsyMQWef-FD0/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/palkesh-malviya/",
+  email:"palkeshmalviyaofficial.108@gmail.com"
+};
+
+export const skills = [
+  {
+    title: "Software Development & Programming",
+    skills: [
+      {
+        name: "Python",
+        image:
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Golang",
+        image:
+          "https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg",
+      },
+      {
+        name: "Flask",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNqtHYopo_XXmaa60pJtj_7yNPnYsWU_KkZmtNyHxkPA&s",
+      },
+      {
+        name: "Django",
+        image:
+          "https://image.spreadshirtmedia.com/image-server/v1/mp/products/T1459A839PA3861PT28D1007202637W10000h5483/views/1,width=800,height=800,appearanceId=839,backgroundColor=F2F2F2/django-logo-sticker.jpg",
+      },
+    ],
+  },
+  {
+    title: "Artificial Intelligence & Data Science",
+    skills: [
+      {
+        name: "Data Analysis",
+        image: "https://w7.pngwing.com/pngs/73/309/png-transparent-yellow-graph-increase-icon-or-logo.png",
+      },
+      {
+        name: "Machine Learning",
+        image:
+        "https://cdn-icons-png.flaticon.com/512/8637/8637099.png",
+      },
+      {
+        name: "Deep Learning",
+        image: "https://static.vecteezy.com/system/resources/previews/025/251/990/original/machine-learning-icon-png.png",
+      },
+      {
+        name: "NLP",
+        image:
+          "https://cdn.iconscout.com/icon/premium/png-256-thumb/nlp-8926384-7279481.png",
+      },
+
+      {
+        name: "Large Language Models",
+        image:
+          "https://t3.ftcdn.net/jpg/06/77/55/04/360_F_677550453_HphDR0L3DSuRl33YrbrpYDo5vJw6eRoa.jpg",
+      },
+      {
+        name: "Computer Vision",
+        image:
+          "https://cdn.iconscout.com/icon/premium/png-256-thumb/computer-vision-1524279-1290826.png",
+      },
+      {
+        name: "Generative AI",
+        image: "https://ih1.redbubble.net/image.4911784195.8379/st,small,507x507-pad,600x600,f8f8f8.jpg",
+      },
+      {
+        name: "MLOps principles",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFaHxcXLpqrJXKcj_cXcwQQQiBe659hMlLIn6CuHSk0jEQG3VUG_6tG7aArEMhegsFy1I&usqp=CAU",
+      },
+      {
+        name: "Prompt Engineering",
+        image:
+          "https://businessolution.org/wp-content/uploads/2023/02/Prompt-Engineering.png",
+      },
+    ],
+  },
+  {
+    title: "Cloud Computing & Infrastructure and Tools",
+    skills: [
+      {
+        name: "IBM Cloud",
+        image:
+          "https://logos-world.net/wp-content/uploads/2020/09/IBM-Symbol.jpg",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/AWS_Simple_Icons_AWS_Cloud.svg/2560px-AWS_Simple_Icons_AWS_Cloud.svg.png",
+      },
+      {
+        name: "CI/CD pipelines",
+        image:
+          "https://www.eficode.com/hs-fs/hubfs/Eficode%202020%20site%20images/Hexagons/CI%20CD%20pipeline%20hexagon-2.png?width=600&name=CI%20CD%20pipeline%20hexagon-2.png",
+      },
+      {
+        name: "Cloud Computing",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/4215/4215831.png",
+      },
+      {
+        name: "Containerization",
+        image:
+          "https://assets-global.website-files.com/62bd9427ec73a11840fe243a/647f308a927f9e56261ca9d7_containerization%20preview.jpeg.jpg",
+      },
+      {
+        name: "Microservices",
+        image:
+          "https://runestone.academy/ns/books/published/py4e-int/_images/web_crawler.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://www.svgrepo.com/show/376331/kubernetes.svg",      
+      },
+      
+      {
+        name: "Tekton",
+        image:
+          "https://avatars.githubusercontent.com/u/47602533?s=280&v=4",
+      },
+      {
+        name: "Git",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://img.icons8.com/?size=48&id=0OQR1FYCuA9f&format=png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.svgrepo.com/show/354202/postman-icon.svg",
+      },
+      
+
+    ],
+  },
+  {
+    title: "Data Analytics & Visualization",
+    skills: [
+      {
+        name: "SQL",
+        image:
+          "https://symbols.getvecta.com/stencil_28/61_sql-database-generic.90b41636a8.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/919/919836.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png",
+      },
+      {
+        name: "Oracle",
+        image:
+          "https://media.licdn.com/dms/image/D5612AQGTzGGnu886Vw/article-cover_image-shrink_720_1280/0/1680581578662?e=2147483647&v=beta&t=Jc3XYEXXsZNfua5Dy96AX17G59H2LrEj4mcGb6rWt60",
+      },
+      {
+        name: "Plotly",
+        image:
+          "https://media.licdn.com/dms/image/D5612AQGTzGGnu886Vw/article-cover_image-shrink_720_1280/0/1680581578662?e=2147483647&v=beta&t=Jc3XYEXXsZNfua5Dy96AX17G59H2LrEj4mcGb6rWt60",
+      },
+      {
+        name: "Tableau",
+        image:
+          "https://media.licdn.com/dms/image/D5612AQGTzGGnu886Vw/article-cover_image-shrink_720_1280/0/1680581578662?e=2147483647&v=beta&t=Jc3XYEXXsZNfua5Dy96AX17G59H2LrEj4mcGb6rWt60",
+      },
+      {
+        name: "Power BI",
+        image:
+          "https://media.licdn.com/dms/image/D5612AQGTzGGnu886Vw/article-cover_image-shrink_720_1280/0/1680581578662?e=2147483647&v=beta&t=Jc3XYEXXsZNfua5Dy96AX17G59H2LrEj4mcGb6rWt60",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://logos-world.net/wp-content/uploads/2020/09/IBM-Symbol.jpg",
+    role: "AI Engineer",
+    company: "IBM Cloud, Bengaluru, India",
+    date: "Dec 2023 - Present",
+    desc: `Project : <strong>IBM GenAI-based Audit Assistance Tool/Chatbot</strong>
+          <h5>Objective:</h5>
+          <p>Streamline audits by providing an assistant chatbot to external auditors (e.g., PwC, KPMG) and internal auditors, enabling them to retrieve reports, filter data, and generate visualizations (e.g., bar, line, pie charts) based on their requirements using natural language prompts via the Audit GEN bot.</p>
+          
+          <h5>Responsibilities:</h5>
+          <ul>
+              Designed the project architecture and workflows.
+              Developed an end-to-end application within the Watsonx ecosystem.
+              Deployed a demo web app within IBM using Fyre instance servers.
+          </ul>
+          
+          <h5>Technologies Used:</h5>
+            <ul>
+                Language: Python
+                Python Modules: Pandas, DuckDB, Plotly, Streamlit
+                Cloud Services: IBM Cloud Object Storage, IBM Code Engine, IBM Watsonx, IBM Watson Data
+            </ul>
+          
+          <h5>Achievements:</h5>
+          <ul>
+              Reduced audit delays by 95%, enabling auditors to retrieve reports quickly using natural language.
+              Minimized manual effort and reduced dependency on security and compliance experts.
+          </ul>
+    `,
+    skills: [
+      "IBM Watsonx.ai",
+      "IBM Watsonx.data",
+      "IBM Code engine",
+      "Langchain",
+      "Large Language Model (LLM)",
+      "Large Multi Model (LMM with Vision)",
+      "Python",
+      "Duckdb",
+      "Vectordb {Croma, Milvus}",
+      "Pandas",
+      "Streamlit",
+      "Plotly"
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://logos-world.net/wp-content/uploads/2020/09/IBM-Symbol.jpg",
+    role: "Cloud Developer",
+    company: "IBM Cloud, Bengaluru, India",
+    date: "Dec 2022 - Dec 2023",
+    desc: `Project Title: <strong>IaaS Compliance Automation Datalake & IBM Security and Compliance Center (SCC)</strong>
+
+    <h5>Objective:</h5>
+    <p>Provide security and compliance reports for all services within IBM Cloud, ensuring adherence to industry standards such as HIPAA and GDPR.</p>
+    <p>Create a unified solution for all services to check their security and compliance posture within IBM Cloud.</p>
+    
+    <h5>Responsibilities:</h5>
+    <p>Developed two microservices in Golang:</p>
+    <p>Ingestion Shaper: Modified data formats to enable advanced SQL queries for filtering data, which is then used in Rego policy applications to automate compliance checks.</p>
+    <p>Radisson Cache: Established a caching mechanism using IBM Redis Cache to optimize the performance of frontend websites and dashboards.</p>
+    <p>Developed unit tests in Golang for three microservices, ensuring high-quality and maintainable code.</p>
+    
+    <h5>Technologies Used:</h5>
+    <p>Languages: Python, Golang, Rego</p>
+    <p>Python Modules: NumPy, Pandas, IBM COS SDK, IBM Redis SDK</p>
+    <p>Golang Packages: Gota, IBM COS SDK, JSON, IBM Redis SDK</p>
+    <p>Cloud Services: IBM Cloud Object Storage, IBM Code Engine, IBM Redis Cache</p>
+    <p>Tools: Docker, Git, GitHub, VS Code, OPA Policy Engine</p>
+    
+    <h5>Achievements:</h5>
+    <p>Reduced manual efforts for generating audit reports by 85%, streamlining compliance processes and increasing efficiency.</p>    
+    `,
+    skills: [
+      "IBM Watsonx.ai",
+      "IBM Watsonx.data",
+      "IBM Code engine",
+      "Langchain",
+      "Large Language Model (LLM)",
+      "Large Multi Model (LMM with Vision)",
+      "Python",
+      "Duckdb",
+      "Vectordb {Croma, Milvus}",
+      "Pandas",
+      "Streamlit",
+      "Plotly"
+    ],
+    doc: "",
+  }
+];
+
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cdac.in/img/C-DAC.jpg",
+    school: "Centre for Development of Advanced Computing (C-DAC), Noida",
+    date: "Mar 2022 - Sep 2022",
+    grade: "Distinction",
+    desc: `I hold a Post Graduate Diploma in Artificial Intelligence from CDAC Noida, where I excelled with distinction.<br>
+    Throughout the program, I gained expertise in Python, data analytics, machine learning, deep learning, natural language processing, cloud computing, Docker, Kubernetes, statistics, and Jenkins for CI/CD.<br>
+    Hands-on projects and collaborative efforts refined my problem-solving abilities and communication skills.<br>
+    I'm passionate about leveraging AI to innovate and tackle real-world challenges.<br>`,
+    degree: "PG Diploma in Artificial Intelligence",
+  },
+  {
+    id: 1,
+    img: "https://www.rgpv.ac.in/Images/RGPVLOGO.JPG",
+    school: "Rajiv Gandhi Proudyogiki Vishwavidyalaya",
+    date: "Jun 2015 - Jun 2019",
+    grade: "A",
+    desc: "I am a distinguished graduate holding a Bachelor of Engineering from RGPV University with specialisation in Mechanical Engineering.",
+    degree: "Bachelor of Engineering",
+  },
+];
+
+export const projects = [
+
+  {
+    id: 0,
+    title: "Vasudha (Plant Disease Detection Web Application) | CDAC | Sept 2022",
+    date: "Sept 2022",
+    description:
+    "Developed a web application for farmers to detect plant diseases through image analysis, with organic and non-organic remediation options. Provided crop and fertilizer recommendations based on soil condition and geographical location.",
+    image:
+      "https://www.appletechsoft.com/wp-content/uploads/2020/08/Impact-of-Mobile-Apps-on-Agriculture-Industry.jpg",
+    tags: ["Python","Flask", "TensorFlow", "scikit-learn", "OpenCV","Deep Learning","AWS","Heroku","Pandas","Numpy"],
+    category: "Artificial Intaligence",
+    github: "https://github.com/Palkesh-7/vasudha_care_project",
+    webapp: "https://github.com/Palkesh-7/vasudha_care_project",
+  },
+  {
+    id: 1,
+    title: "Split Bill (Expenditure Monitoring and Bill Splitting Web Application)",
+    date: "May 2023",
+    description:
+        "Developed a web application to help individuals and groups monitor their expenses, split bills, and receive monthly reports and notifications for better financial management.",
+    image:
+      "https://redliodesigns.com/_next/image?url=https%3A%2F%2Ffiles.redliodesigns.com%2Ffiles%2FInspirations%2Fbill-payment-app.jpg&w=1200&q=75",
+    tags: ["Python","Flask","Djengo","AWS","Heroku","Pandas","Numpy"],
+    category: "web app",
+    github: "https://github.com/Palkesh-7/Split-bill-Python",
+    webapp: "https://github.com/Palkesh-7/Split-bill-Python",
+  },
+  {
+    id: 2,
+    title: "MediQuick Cure - Medical Appointment Booking System",
+    date: "March 2023",
+    description:
+      "Developed a comprehensive medical appointment booking system aimed at simplifying healthcare services. MediQuick Cure enables users to book appointments with various medical professionals (e.g., doctors, pathologists, nurses), while also offering medical professionals a seamless environment to manage and schedule their appointments.",
+    image:
+      "https://simplybook.me/build/images/trial-section/medical.7358dc7d.png",
+    tags: [
+      " Golang", "Gin (HTTP Web Framework)", "MySQL","MongoDB","Postman", "Git","GitHub", "VS Code",
+    ],
+    category: "machine learning",
+    github: "https://github.com/Palkesh-7/das-09-07/tree/main/Doctor-Appointment-Project",
+    webapp: "https://github.com/Palkesh-7/das-09-07/tree/main/Doctor-Appointment-Project",
+  },
+
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
